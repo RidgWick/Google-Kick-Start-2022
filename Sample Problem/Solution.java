f31b27245ec9cb44618c59c380148377bfb8328d
Md _ Rifat ( 0x0mmrhr ) <81492392+RidgWick@users.noreply.github.com>
2022-02-19 03:52:11 +0000

    Update Solution.java

--- Sample Problem/Solution.java
+++ Sample Problem/Solution.java
@@ -1,1 +1,26 @@
+// Author : MD_Rifat(0x0mmrhr)
+// Email : 0x0mmrhr.ridgwick@cyberdude.com
+// Twitter : https://twitter.com/ridg__wick
 
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        long testCases = in.nextInt();
+        int N, M;
+        for (int Case = 1; Case <= testCases; Case++) {
+
+            N = in.nextInt();
+            M = in.nextInt();
+            int sumOfAllCandies = 0;
+            
+            for (int i = 0; i < N; ++i) {
+                sumOfAllCandies += in.nextInt();
+            }
+
+            System.out.println("Case #" + Case + ": " + (sumOfAllCandies % M) );
+        }
+    }
+}
